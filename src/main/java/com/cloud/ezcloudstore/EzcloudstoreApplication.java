67a9a6fd2607d6da9709d1d44c667393e56e013a
rohanbhadreshpatel <rohanbpatel98@gmail.com>
2024-12-26 02:21:39 -0600

    chore: initial commit - MSSE cloud technologies project with updated pom.xml

---
+++ src/main/java/com/cloud/ezcloudstore/EzcloudstoreApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.ezcloudstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzcloudstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzcloudstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/ezcloudstore/EzcloudstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.ezcloudstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzcloudstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
